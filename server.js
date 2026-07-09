e99cc8e34590d3b0a8a04827b813203bb803bbd8
Vasco Manservigi <vasco@MacBook-Air-di-Vasco.local>
2026-07-09 18:57:38 +0200

    Aggiunto supporto per form-encoded data da Python requests

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.text());
 app.use(express.raw({ type: '*/*' }));
 
